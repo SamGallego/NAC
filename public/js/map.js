a65620895a47f2eb52563f38e673a052676b8697
kevinu27 <kevin.sm89@gmail.com>
2021-07-07 15:54:28 +0100

    primer commit

--- public/js/map.js
+++ public/js/map.js
@@ -1,51 +1,20 @@
-
-// Initialize and add the map
+let map, marker
 function initMap() {
-    // The location of Uluru
     const uluru = { lat: 28.093491, lng: -14.279944 }
-    // The map, centered at Uluru
-    const map = new google.maps.Map(document.getElementById("map"), {
+
+    map = new google.maps.Map(document.getElementById("map"), {
         zoom: 12,
         center: uluru,
     })
-    // The marker, positioned at Uluru
-    const contentString =
-        '<div id="content">' +
-        '<div id="siteNotice">' +
-        "</div>" +
-        '<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
-        '<div id="bodyContent">' +
-        "<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large " +
-        "sandstone rock formation in the southern part of the "
-
-    const infowindow = new google.maps.InfoWindow({
-        content: contentString,
-    })
-
-    const marker = new google.maps.Marker({
-        // position: uluru,
-        // map: map,
-        // title: 'uwu'
-    })
-
-
-    marker.addListener("click", () => {
-        infowindow.open({
-            anchor: marker,
-            map,
-            shouldFocus: false,
-        })
-    })
 
     function addMarker(location) {
-        const marker = new google.maps.Marker({
+        if (marker) marker.setMap(null)
+
+        marker = new google.maps.Marker({
             position: location,
             map: map,
         });
-        // markers.push(marker);
     }
-
-
 
     map.addListener("click", (event) => {
         addMarker(event.latLng);
@@ -55,14 +24,4 @@ function initMap() {
         const inputLng = document.getElementById('lng')
         inputLng.value = event.latLng.lng()
     });
-
-
-
-
-
-
-
-
 }
-
-
